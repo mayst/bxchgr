de25f1c32c4fc672d66290a02fabac67e8857765
Denis Kurilyov <otrawaotrawa@gmail.com>
2023-07-19 23:51:38 +0300

    feat/multiple-select

---
+++ src/components/inputs/MultipleSelectBx.tsx
@@ -0,0 +1,66 @@
+import { Box, Chip, FormControl, InputLabel, MenuItem, Select } from "@mui/material"
+
+type Option = {
+  id: number
+  name: string,
+  selected: boolean
+}
+
+interface ISelectBxProps {
+  label: string
+  onChange: (id: number) => void
+  options: Option[]
+  fullWidth?: boolean
+}
+
+export const MultipleSelectBx =
+  (props: ISelectBxProps) => {
+    const { label, onChange, options, fullWidth } = props
+
+    const selectId = label + '-select'
+
+    const selected = options
+      .filter(opt => opt.selected)
+      .map(opt => opt.name)
+
+    return (
+      <>
+        <FormControl fullWidth={fullWidth}>
+          <InputLabel id={selectId + '-label'}>{label}</InputLabel>
+          <Select
+            multiple
+            labelId={selectId + '-label'}
+            id={selectId}
+            value={selected}
+            label={label}
+          >
+            {options.map(opt => (
+              <MenuItem
+                key={opt.name}
+                value={opt.name}
+                onClick={() => onChange(opt.id)}
+              >
+                {opt.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Box
+          display='flex'
+          flexWrap='wrap'
+          gap={1}
+          mt={2}
+        >
+          {options
+            .filter(opt => opt.selected)
+            .map(opt => (
+              <Chip
+                key={opt.name}
+                label={opt.name}
+                onDelete={() => onChange(opt.id)}
+              />
+            ))}
+        </Box>
+      </>
+    )
+  }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,55 @@
+import { useState } from "react"
+import { MultipleSelectBx } from "../components/inputs/MultipleSelectBx"
+
 export const HomePage = () => {
+  const [genres, setGenres] = useState([
+    {
+      id: 0,
+      name: 'Thriller',
+      selected: true
+    },
+    {
+      id: 1,
+      name: 'Fantasy',
+      selected: false
+    },
+    {
+      id: 2,
+      name: 'Mystery',
+      selected: true
+    },
+    {
+      id: 3,
+      name: 'Dystopian',
+      selected: false
+    },
+    {
+      id: 4,
+      name: 'Romance',
+      selected: false
+    },
+    {
+      id: 5,
+      name: 'Westerns',
+      selected: false
+    },
+  ])
+
+  const handleSelectGenre = (id: number) => {
+    setGenres(prev => prev.map(
+      genre => genre.id !== id
+        ? genre
+        : { ...genre, selected: !genre.selected }))
+  }
+
   return (
-    <div>HomePage</div>
+    <div>
+      <MultipleSelectBx
+        fullWidth
+        label="Genres"
+        options={genres}
+        onChange={handleSelectGenre}
+      />
+    </div>
   )
 }
